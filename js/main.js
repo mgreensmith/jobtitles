b22c4e9be39ba572873b377446e1f029372113d1
Matt Greensmith <matt.greensmith@gmail.com>
2015-12-15 10:08:31 -0800

    working implementation

---
+++ js/main.js
@@ -0,0 +1,64 @@
+/**
+This is based on TitleCreator.py, created by gabe@abinante.com
+
+Source mirrored at https://gist.github.com/mgreensmith/c427ad924a7291faf35c
+*/
+
+var jobCategories = ['Solutions', 'Systems', 'Network', 'Security', 'Compliance', 'Information', 'Scalability'];
+
+var possessivePrefixes = ['Viceroy','Commandant', 'Grand Poo-Bah', 'Archon', 'Duke', 'Chancellor', 'President', 'Marquis', 'Earl', 'Director', 'Chair', 'Head', 'Senior Director'];
+var possessiveSuffixes = ['Engineering', 'Management', 'Development', 'Deployment', 'Technical Training', 'Operations', 'Architecture', 'Infrastructure', 'Technology', 'Administration', 'Management', 'Computational Analytics', 'Database Administration'];
+
+var adjectivePrefixes = ['Principal','Chief','Head','Lead', 'Senior', 'Master'];
+var adjectiveSuffixes = ['Engineer', 'Architect', 'Designer', 'Consultant', 'Manager', 'Officer'];
+
+var romanGrades = ['I', 'II', 'III', 'IV'];
+
+
+/**
+ * Utility to wrap the different behaviors between W3C-compliant browsers
+ * and IE when adding event handlers.
+ *
+ * @param {Object} element Object on which to attach the event listener.
+ * @param {string} type A string representing the event type to listen for
+ *     (e.g. load, click, etc.).
+ * @param {function()} callback The function that receives the notification.
+ */
+function addListener(element, type, callback) {
+ if (element.addEventListener) element.addEventListener(type, callback);
+ else if (element.attachEvent) element.attachEvent('on' + type, callback);
+}
+
+function randomBool() {
+  return Math.random()<.5;
+}
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function generateTitle() {
+  if (randomBool()) {
+    title = randomElement(possessivePrefixes) + ' of ' + randomElement(jobCategories) + ' ' + randomElement(possessiveSuffixes)
+  } else {
+    title = randomElement(adjectivePrefixes) + ' ' + randomElement(jobCategories) + ' ' + randomElement(adjectiveSuffixes)
+  }
+
+  if (randomBool()) {
+    title = title + ' ' + randomElement(romanGrades)
+  }
+
+  return title;
+}
+
+function updateTitle() {
+  document.getElementById("name").innerHTML = generateTitle();
+}
+
+updateTitle();
+
+var newTitleLink = document.getElementById('again');
+
+addListener(newTitleLink, 'click', function() {
+  updateTitle();
+});
